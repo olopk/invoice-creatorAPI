b5ed5154dcbc71088e30b959dad99b0121b4c4a5
olopk <olekwojas@gmail.com>
2020-03-25 22:36:10 +0100

    first graphql schema

--- graphql/schema.js
+++ graphql/schema.js
@@ -1,16 +1,39 @@
 const { buildSchema } = require('graphql');
 
 module.exports = buildSchema(`
-
-    type TestData{
-        text: String!,
-        views: Int!
+    input Customer{
+        name: String!
+        nip: Int!
+        city: String!
+        street: String!
+    }
+    input Order{
+        name: String!
+        unit_price: Float!
+        total_price: Float!
+        quantity: Int!
+    }
+    input InvoiceInputData {
+        invoice_nr: String!
+        date: String!
+        total_price: Float!
+        customer: Customer!
+        order: [Order!]!
+    }
+    type retObj{
+        message: String!
     }
 
     type RootQuery{
-        hello: TestData!
+        getInvoices: String!
     }
+
+    type RootMutation{
+        addInvoice(invoiceInput: InvoiceInputData): retObj!
+    }
+
     schema{
         query: RootQuery
+        mutation: RootMutation
     } 
 `);
