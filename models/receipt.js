cd2e2c1fdf14a5668e7d08ba92dcf6e8ec765f8a
olopk <olekwojas@gmail.com>
2020-05-14 22:04:01 +0200

    added receipt mongoose model.

---
+++ models/receipt.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const moment = require('moment-timezone');
+const WarsawTimezone = moment.tz(Date.now(), 'Europe/Warsaw')
+
+const receiptSchema = new Schema({
+    receipt_nr: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: WarsawTimezone,
+        required: true
+    },
+    customer: {
+        type: Schema.Types.ObjectId,
+        ref: 'Customer',
+        required: false
+    },
+    order: [
+        {
+            product:{
+                type: Schema.Types.ObjectId,
+                ref: 'Product',
+                required: true
+            },
+            quantity: {
+                type: Number,
+                required: true
+            },
+            price_net:{
+                type: Number,
+                required: true
+            },
+            total_price_net:{
+                type: Number,
+                required: true
+            },
+            price_gross:{
+                type: Number,
+                required: true
+            },
+            total_price_gross:{
+                type: Number,
+                required: true
+            },
+            vat:{
+                type: Number,
+                required: true
+            }
+        }
+    ],
+    total_price:{
+        type: Number,
+        required: true
+    }
+})
+
+module.exports = mongoose.model('Receipt', receiptSchema)
