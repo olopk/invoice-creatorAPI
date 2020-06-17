46527020b20f6f7c8eb427a1b832877c50c32f29
olopk <olekwojas@gmail.com>
2020-06-17 16:50:50 +0200

    Auth fix.

--- middlewares/Auth.js
+++ middlewares/Auth.js
@@ -1,34 +1,35 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) =>{
-    // const authHeader = req.get('Authorization');
-    // if(!authHeader){
-    //     req.logged = false;
-    //     next()
-    // }else{
-    //     const token = authHeader.split(' ')[1];
-    //     let decodedToken;
-    //     try{
-    //         decodedToken = jwt.verify(token, 'UltrasecretOptyk')
-    //     }catch(err){
-    //         err.message = "Token uległ wygaśnięciu, zaloguj się ponownie"
-    //         err.statusCode = 401    
-    //         throw err
-    //     }
-    //     if(!decodedToken){
-    //         const error = new Error('Brak autotyzacji.')
-    //         error.statusCode = 401;
-    //         throw error
-    //     }
-        // req.userData = {
-        //     userId: decodedToken.userId,
-        //     name: decodedToken.name
-        // }
-        req.userData = {
-            userId: 1234,
-            name: 'Test'
-        }
-        req.logged = true;
+    const authHeader = req.get('Authorization');
+    if(!authHeader){
+        req.logged = false;
         next()
-    // }
+    }else{
+        const token = authHeader.split(' ')[1];
+        let decodedToken;
+        try{
+            decodedToken = jwt.verify(token, 'UltrasecretOptyk')
+        }catch(err){
+            err.message = "Token uległ wygaśnięciu, zaloguj się ponownie"
+            err.statusCode = 401    
+            throw err
+        }
+        if(!decodedToken){
+            const error = new Error('Brak autotyzacji.')
+            error.statusCode = 401;
+            throw error
+        }
+        req.userData = {
+            userId: decodedToken.userId,
+            name: decodedToken.name
+        }
+        
+        // req.userData = {
+        //     userId: 1234,
+        //     name: 'Test'
+        // }
+        // req.logged = true;
+        // next()
+    }
 }
